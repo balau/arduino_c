06c3064a1b871dc8eea806044f1009c1dc8a2984
Francesco Balducci <balau@users.sourceforge.net>
2014-10-18 14:33:42 +0200

    Added LED blinking example.

---
+++ blink/blink.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2014 Francesco Balducci
+ *
+ * This file is part of arduino_c.
+ *
+ *    arduino_c is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    embedded-snippets is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with embedded-snippets.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <avr/io.h>
+#include <util/delay.h>
+
+enum {
+	BLINK_DELAY_MS = 1000,
+};
+
+int main (void)
+{
+	/* set pin 5 of PORTB for output*/
+	DDRB |= _BV(DDB5);
+
+	while(1) {
+		/* set pin 5 high to turn led on */
+		PORTB |= _BV(PORTB5);
+		_delay_ms(BLINK_DELAY_MS);
+
+		/* set pin 5 low to turn led off */
+		PORTB &= ~_BV(PORTB5);
+		_delay_ms(BLINK_DELAY_MS);
+	}
+
+	return 0;
+}
+
